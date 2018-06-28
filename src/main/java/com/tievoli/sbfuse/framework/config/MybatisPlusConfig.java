ee304328f97f105c572596a0b98694a06fab0174
jiangbin <jiangbin@iciyun.com>
2018-06-28 21:05:17 +0800

    添加SQL配置.

--- src/main/java/com/tievoli/sbfuse/framework/config/MybatisPlusConfig.java
+++ src/main/java/com/tievoli/sbfuse/framework/config/MybatisPlusConfig.java
@@ -1,7 +1,10 @@
 package com.tievoli.sbfuse.framework.config;
 
+import com.baomidou.mybatisplus.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * MybatisPlus JavaConfig.
@@ -10,5 +13,13 @@ import org.springframework.context.annotation.Configuration;
 @MapperScan(basePackages = "com.tievoli.sbfuse.biz.**.*Mapper")
 public class MybatisPlusConfig {
 
+    /**
+     * mybatis-plus SQL执行效率插件【生产环境可以关闭】
+     */
+    @Bean
+    @Profile("dev")
+    public PerformanceInterceptor performanceInterceptor() {
+        return new PerformanceInterceptor();
+    }
 
 }
